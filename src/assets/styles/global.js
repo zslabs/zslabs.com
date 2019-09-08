31884170b9cdc7a509dfc228dea96f72bb04eca0
Zach Schnackel <info@zslabs.com>
2019-09-07 22:57:26 -0400

    add in some global styles

--- src/assets/styles/global.js
+++ src/assets/styles/global.js
@@ -1,4 +1,41 @@
-export const global = () => ({
+import { rgba } from 'polished';
+import { generateGradient } from 'chaoskit/src/assets/styles/utility/gradient';
+
+import pattern from '../media/pattern.png';
+import bubbles from '../media/bubbles.svg';
+
+export const global = theme => ({
+  html: {
+    position: 'relative',
+
+    '&::before': {
+      content: "''",
+      width: '100%',
+      height: 200,
+      position: 'absolute',
+      top: '-$global-whitespace-large',
+      left: '0',
+      background: `url(${pattern})`,
+      maskImage: `url(${bubbles})`,
+      zIndex: '-2',
+    },
+
+    '&::after': {
+      content: "''",
+      position: 'absolute',
+      top: 0,
+      left: 0,
+      width: '100%',
+      height: 200,
+      background: generateGradient({
+        start: theme.color.light.base,
+        stop: rgba(theme.color.light.base, 0.85),
+        position: 'to bottom',
+      }),
+      zIndex: -1,
+    },
+  },
+
   // Reset `letterSpacing` Chaoskit does by default
   'h1, h2, h3, h4, h5': {
     letterSpacing: 'normal',
--- src/layouts/Foundation.js
+++ src/layouts/Foundation.js
@@ -46,6 +46,28 @@ const zslabsTheme = extendTheme(theme, {
         'invert(36%) sepia(94%) saturate(1614%) hue-rotate(194deg) brightness(103%) contrast(92%)',
     },
   },
+  warning: {
+    base: '#fcd000',
+    get light() {
+      return tint(0.95, this.base);
+    },
+    get dark() {
+      return shade(0.05, this.base);
+    },
+    filter:
+      'invert(80%) sepia(79%) saturate(3799%) hue-rotate(354deg) brightness(97%) contrast(105%)',
+  },
+  danger: {
+    base: '#f25041',
+    get light() {
+      return tint(0.95, this.base);
+    },
+    get dark() {
+      return shade(0.05, this.base);
+    },
+    filter:
+      'invert(64%) sepia(92%) saturate(5728%) hue-rotate(339deg) brightness(101%) contrast(90%)',
+  },
   fontFamily: {
     base: "Calibre, 'Helvetica Neue', Arial, sans-serif",
     heading: "Calibre, 'Helvetica Neue', Arial, sans-serif",
