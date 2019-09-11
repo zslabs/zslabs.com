c49590edf537e97f267776059d9761381e37d853
Zach Schnackel <info@zslabs.com>
2019-09-11 14:34:30 -0400

    globals

--- src/assets/styles/global.js
+++ src/assets/styles/global.js
@@ -1,4 +1,4 @@
-import { rgba } from 'polished';
+import { rgba, padding } from 'polished';
 import { generateGradient } from 'chaoskit/src/assets/styles/utility/gradient';
 
 import pattern from '../media/pattern.png';
@@ -13,7 +13,7 @@ export const global = theme => ({
       width: '100%',
       height: 200,
       position: 'absolute',
-      left: '0',
+      left: 0,
     },
 
     '&::before': {
@@ -31,6 +31,45 @@ export const global = theme => ({
         position: 'to bottom',
       }),
       zIndex: -1,
+    },
+  },
+
+  // Article specific
+  '.svgBackground-example': {
+    width: 50,
+  },
+
+  '.filename': {
+    marginTop: theme.space.base,
+    background: theme.color.dark.base,
+    color: theme.contrast.base,
+    fontSize: theme.fontSize.small,
+    fontFamily: theme.fontFamily.code,
+    ...padding(theme.space.xsmall, theme.space.base),
+    borderTopLeftRadius: theme.borderRadius.base,
+    borderTopRightRadius: theme.borderRadius.base,
+
+    '+ .gatsby-highlight pre': {
+      borderTopLeftRadius: 0,
+      borderTopRightRadius: 0,
+    },
+  },
+
+  //
+  // CodePen Embeds
+  // `gatsby-remark-codepen`
+  //
+
+  '.codepenEmbed': {
+    position: 'relative',
+    paddingBottom: '56.25%',
+
+    iframe: {
+      position: 'absolute',
+      top: 0,
+      left: 0,
+      width: '100%',
+      height: '100%',
     },
   },
 });
