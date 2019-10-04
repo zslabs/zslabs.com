b5a5cce237e5cead80a2ed5d8f48c7d8adf652f1
Zach Schnackel <info@zslabs.com>
2019-10-04 17:20:12 -0400

    Fix SSR

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,14 +1,14 @@
 import PropTypes from 'prop-types';
 import { ThemeProvider } from 'emotion-theming';
 import { CacheProvider } from '@emotion/core';
-import { ckCache } from 'chaoskit/src/helpers/Wrapper';
+import { createCKCache } from 'chaoskit/src/helpers/Wrapper';
 
 import { ZSProvider } from './src/components/ZSContext';
 import { zslabsTheme } from './utils/theme';
 
 export const wrapRootElement = ({ element }) => (
   <ZSProvider>
-    <CacheProvider value={ckCache}>
+    <CacheProvider value={createCKCache()}>
       <ThemeProvider theme={zslabsTheme}>{element}</ThemeProvider>
     </CacheProvider>
   </ZSProvider>
