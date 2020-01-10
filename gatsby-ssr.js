c1e743c2f5fbec16e13a50eddf521adf43312e67
Zach Schnackel <info@zslabs.com>
2020-01-09 21:43:37 -0500

    Update SSR strategy

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,17 +1,44 @@
 import PropTypes from 'prop-types';
+import { renderToString } from 'react-dom/server';
 import { ThemeProvider } from 'emotion-theming';
 import { CacheProvider } from '@emotion/core';
+import createEmotionServer from 'create-emotion-server';
 import { createCKCache } from 'chaoskit/src/helpers/Wrapper';
 
 import { ZSProvider } from './src/components/ZSContext';
+
 import { zslabsTheme } from './utils/theme';
 
-export const wrapRootElement = ({ element }) => (
-  <ZSProvider>
-    <CacheProvider value={createCKCache()}>
-      <ThemeProvider theme={zslabsTheme}>{element}</ThemeProvider>
+export const replaceRenderer = ({
+  replaceBodyHTMLString,
+  bodyComponent,
+  setHeadComponents,
+}) => {
+  const cache = createCKCache();
+  const { extractCritical } = createEmotionServer(cache);
+
+  const element = (
+    <CacheProvider value={cache}>
+      <ThemeProvider theme={zslabsTheme}>{bodyComponent}</ThemeProvider>
     </CacheProvider>
-  </ZSProvider>
+  );
+
+  const { html, css, ids } = extractCritical(renderToString(element));
+
+  setHeadComponents([
+    <style
+      data-emotion-css={ids.join(' ')}
+      dangerouslySetInnerHTML={{
+        __html: css,
+      }}
+    />,
+  ]);
+
+  replaceBodyHTMLString(html);
+};
+
+export const wrapRootElement = ({ element }) => (
+  <ZSProvider>{element}</ZSProvider>
 );
 
 wrapRootElement.propTypes = {
