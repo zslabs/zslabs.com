7cc042f31008e855424fa0a433d3794fee317b77
Zach Schnackel <info@zslabs.com>
2020-11-18 13:32:32 -0500

    update SSR

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,15 +1,38 @@
-import { CacheProvider, ThemeProvider } from '@emotion/react'
 import { createCKCache } from 'chaoskit/src/helpers/Wrapper'
 import { MDXProvider } from '@mdx-js/react'
+import { CacheProvider, ThemeProvider } from '@emotion/react'
+import { renderToString } from 'react-dom/server'
+import createEmotionServer from '@emotion/server/create-instance'
 
 import { zslabsTheme } from './utils/theme'
 
 import { shortcodes } from '~components/mdxShortcodes'
 
-export const wrapRootElement = ({ element }) => (
-  <CacheProvider value={createCKCache()}>
-    <ThemeProvider theme={zslabsTheme}>
-      <MDXProvider components={shortcodes}>{element}</MDXProvider>
-    </ThemeProvider>
-  </CacheProvider>
-)
+export const replaceRenderer = ({
+  replaceBodyHTMLString,
+  bodyComponent,
+  setHeadComponents,
+}) => {
+  const cache = createCKCache()
+  const { extractCritical } = createEmotionServer(cache)
+
+  const element = (
+    <CacheProvider value={cache}>
+      <ThemeProvider theme={zslabsTheme}>
+        <MDXProvider components={shortcodes}>{bodyComponent}</MDXProvider>
+      </ThemeProvider>
+    </CacheProvider>
+  )
+  const { html, ids, css } = extractCritical(renderToString(element))
+
+  setHeadComponents([
+    <style
+      key="emotion"
+      data-emotion-css={ids.join(' ')}
+      dangerouslySetInnerHTML={{
+        __html: css,
+      }}
+    />,
+  ])
+  replaceBodyHTMLString(html)
+}
